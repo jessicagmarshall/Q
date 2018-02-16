1cafda098655371efd7b00e522ebef3733ab0964
Jessica Marshall <jessica.marshall14@gmail.com>
2018-02-16 16:51:39 -0700

    extension opens coinmarketcap on click

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href")
-
-console.log(firstHref)
-
-alert("Hello from your Chrome extension!")
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    // console.log(request.message)
+    if (request.message === 'clicked_browser_action') {
+      var firstHref = $("a[href^='http']").eq(0).attr('href')
+      // console.log(firstHref)
+      chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'https://coinmarketcap.com/'})
+    }
+  }
+)
