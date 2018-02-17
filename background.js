f06a00007f222f9a6c7f39cc42904799557ccafe
Jessica Marshall <jessica.marshall14@gmail.com>
2018-02-16 17:54:01 -0700

    YOU CANNOT ESCAPE COINMARKETCAP

--- background.js
+++ background.js
@@ -1,18 +1,44 @@
 // Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.tabs.onActivated.addListener(function (tab) {
   // Send a message to the active tab
-  // console.log('GOOBI')
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-    let activeTab = tabs[0]
-    // console.log(tabs)
-    // console.log(activeTab)
-    chrome.tabs.sendMessage(activeTab.id, {'message': 'clicked_browser_action'})
+  chrome.tabs.query({}, function (tabs) {
+    console.log(tabs)
+    // if you're not currently doing things on coinmarketcap
+    if (tabs[0].url !== 'https://coinmarketcap.com/') {
+      let i
+
+      // check to see if there's a coinmarketcap instance open already
+      for (i = 0; i < tabs.length; i++) {
+        // if so, go there
+        if (tabs[i].url === 'https://coinmarketcap.com/') {
+          chrome.tabs.highlight({tabs: i}, function () {
+
+            /////////// JUST FOR TESTING ///////////
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+              // check that this is coinmarketcap
+              console.log('part 1', tabs[0].url)
+            })
+            /////////// JUST FOR TESTING ///////////
+
+          })
+        }
+      }
+      // at this point, if there is a coinmarketcap page, you're there
+      // if you're still not there, get your butt there
+      chrome.tabs.query({active: true, currentWindow: true}, function (newTabs) {
+        if (newTabs[0].url !== ('https://coinmarketcap.com/' || 'chrome://newtab/')) {
+          console.log('part 2', newTabs[0].url)
+          chrome.tabs.create({'url': 'https://coinmarketcap.com/'})
+        }
+      })
+    }
   })
 })
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === 'open_new_tab') {
-      chrome.tabs.create({'url': request.url})
-    }
-  })
+//
+// chrome.runtime.onMessage.addListener(
+//   function (request, sender, sendResponse) {
+//     if (request.message === 'open_new_tab') {
+//       chrome.tabs.create({'url': request.url})
+//     }
+//   })
--- content.js
+++ content.js
@@ -1,10 +1,8 @@
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    // console.log(request.message)
-    if (request.message === 'clicked_browser_action') {
-      var firstHref = $("a[href^='http']").eq(0).attr('href')
-      // console.log(firstHref)
-      chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'https://coinmarketcap.com/'})
+    if (request.message === 'loaded_page') {
+      console.log('loaded page')
+      // chrome.runtime.sendMessage({'message': 'open_new_tab', 'url': 'https://coinmarketcap.com/'})
     }
   }
 )
